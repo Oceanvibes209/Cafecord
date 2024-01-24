29c595e74ded64729e054905bf215a91c9a0ba42
Alexandro Aguilera <navysealalex@gmail.com>
2024-01-24 13:06:55 -0800

    Messages save to local browser, and avatar selection works

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Login from './Pages/Login';
 import Chat from './Pages/Chat';
 import EditProfile from './Pages/EditProfile';
+import AvatarContext from './Pages/AvatarContext';
 
 
 function App() {
   const location = useLocation();
+  const [selectedAvatar, setSelectedAvatar] = useState('/Avatars/Beeo-o.jpg');
+
   return (
+    <AvatarContext.Provider value={{ selectedAvatar, setSelectedAvatar }}>
     <Routes key={location.pathname} location={location} >
       <Route index element={<Login />} />
       <Route path='/Login' element={<Login />} />
       <Route path="/Chat" element={<Chat />} />
       <Route path='/EditProfile' element={<EditProfile />}/>
     </Routes>
+    </AvatarContext.Provider>
   )
 }
 
