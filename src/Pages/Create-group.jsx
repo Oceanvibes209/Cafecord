ce593004823501e3a2f70d104cc661e6b82a3e3e
Tania Ali <taniafali@gmail.com>
2024-01-28 13:54:27 -0800

    Refactor Form and change styles

--- src/Pages/Create-group.jsx
+++ src/Pages/Create-group.jsx
@@ -9,30 +9,51 @@ import Stack from 'react-bootstrap/Stack';
 import profileImage from '../assets/profile-image.png';
 import { v4 as uuidv4 } from 'uuid';
 import { TiDelete } from "react-icons/ti";
+import { IoMdAdd } from "react-icons/io";
 
 const CreateGroup = () => {
+    const [formData, setFormData] = useState({
+        groupName: "",
+        description: "",
+        members: [],
+    });
+    const handleChange = (e)=> {
+       let {name, value} = e.target;
+       setFormData(prevData => (
+        {
+            ...prevData,
+        [name]: value
+        } ))
+    }
     const usernameRef = useRef(null);
-    const [members, setMembers] = useState([]);
+    const groupNameRef = useRef(null);
+    const descriptionRef =useRef(null);
+    const addMemBtnRef = useRef(null);
+    const addSubmitBtnRef = useRef(null);
     const handleAddMember = () => {
         const memberId = uuidv4();
-        const username = usernameRef.current.value;
-        if (!username) {
-            return;
-        }
-        usernameRef.current.value = '';
-        // search for username in the database and get member object with the same username as below:
-        //  if username found
-        const member = {
-            id: memberId,
-            username: username,
-            firstName: 'abc',
-            lastName: 'xyz',
-            imageUrl: profileImage,
-            messages: []
-        };
-        setMembers([...members, member]);
-        // else throw error, "User not found!".
-
+            const username = usernameRef.current.value;
+            if (!username) {
+                return;
+            }
+            usernameRef.current.value = '';
+            // search for username in the database and get member object with the same username as below:
+            //  if username found
+            const member = {
+                id: memberId,
+                username: username,
+                firstName: 'abc',
+                lastName: 'xyz',
+                imageUrl: profileImage,
+                messages: []
+            };
+            setFormData(prevData => (
+                {
+                    ...prevData,
+                    members: [...prevData.members, member]
+                }
+            ))
+            // else throw error, "User not found!".
     }
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -41,74 +62,115 @@ const CreateGroup = () => {
             e.stopPropagation();
             alert("Group name is required!");
             return;
-        }
-        const newGroup = {
-            id: uuidv4(),
-            name: e.currentTarget.name.value,
-            description: e.currentTarget.description.value,
-            members: members,
-            messages: []
         };
-        alert(`Created a group with the name ${newGroup.name}!
+        const newGroup = {id: uuidv4(), ...formData};
+        console.log(newGroup)
+        // save new group in database then alert;
+        alert(`Created a group with the name ${newGroup.groupName}!
          
          ${JSON.stringify(newGroup)};
         `);
     }
     const handleDeleteMember = (id) => {
-        const updatedMembers = members.filter(member => member.id !== id);
-        setMembers(updatedMembers);
+        const updatedMembers = formData.members.filter(member => member.id !== id);
+        setFormData(prevData => (
+            {...prevData, 
+                members: updatedMembers
+            }
+        ))
     }
     const renderMembers = () => {
-        return members.map((member) => (
+        return formData.members.map((member) => (
             <div key={member.id}>
                 <div className={Styles.profileImgContainer} onClick={() => handleDeleteMember(member.id)}>
-                    <img className={'rounded-circle' + ' ' + Styles.profileImage} src={member.imageUrl} alt="profile image" width="65px" height="65px" />
+                    <img className={Styles.profileImage} src={member.imageUrl} alt="profile image" width="65px" height="65px" />
                     <TiDelete className={Styles.deleteIcon} size={34} />
                 </div>
                 <div className="fw-bold">{member.username}</div>
             </div>
         ))
     }
+    const checkKeyDown = (e) => {
+        if (e.keyCode == 13 ) {
+            e.preventDefault();
+            if(e.target.name === 'groupName' && e.target.value) {
+                descriptionRef.current.focus();
+             } else if (e.target.name === "description"){
+                 usernameRef.current.focus();
+             } else if (e.target.name === 'username'){
+                 addMemBtnRef.current.focus();
+             } else if (e.target.id === 'add-member'){
+                 handleAddMember();
+                 addSubmitBtnRef.current.focus();
+             } else if (e.target.id === 'new-group'){
+                handleSubmit(e);
+                return true;
+             }
+        } else {
+            return true;
+        }
+       
+    }
+    const handleBlur = (e)=> {
+        e.target.addClass("input-blur");
+    }
     return (
-        <Container className={Styles.body}>
-            <Row className={Styles.container}>
-                <Col sm={12} md={6} lg={6} >
-                    <Form id="new-group" onSubmit={handleSubmit} className={'d-md-flex flex-md-column justify-content-md-center h-100' + ' ' + Styles.form}>
-                        <h2>Create a Group</h2>
-                        <Row>
-                            <Col>
+        <Container fluid className={Styles.body}>
+            <Row className={Styles.container + " m-0"}>
+                <Col className={Styles.formContainer} sm={12} md={6} lg={6} >
+                    <h2 className={Styles.heading}>Create a Group</h2>
+                    <Form id="new-group" 
+                          className={'d-md-flex flex-md-column justify-content-md-center mx-2' + ' ' + Styles.form} 
+                          onSubmit={handleSubmit} 
+                          onKeyDown={(e) => checkKeyDown(e)}
+                        >
+                        <Row className="mx-0">
+                            <Col className="px-0">
                                 <Form.Control
                                     className={Styles.inputEl}
                                     required
                                     type="text"
                                     placeholder="Group Name"
-                                    name="name"
+                                    name="groupName"
+                                    value={formData.groupName}
+                                    id={uuidv4() + "-groupName"}
+                                    onChange = {handleChange}
+                                    ref={groupNameRef}
+                                    autoFocus={true}
+                                    onBlur={handleBlur}
                                 />
                             </Col>
                         </Row>
-                        <Row>
-                            <Col>
+                        <Row className="mx-0">
+                            <Col className="px-0">
                                 <Form.Control
                                     className={Styles.inputEl}
                                     type="text"
                                     placeholder="Group Description"
                                     name="description"
+                                    value={formData.description}
+                                    id={uuidv4() + "-description"}
+                                    onChange = {handleChange}
+                                    ref={descriptionRef}
 
                                 />
                             </Col>
                         </Row>
-                        <Row>
-                            <Col xs={6} className="pe-0">
+                        <Row className={Styles.usernameInput + " mx-0"}>
+                            <Col className="px-0" xs={8}>
                                 <Form.Control
                                     className={Styles.inputEl}
                                     type="text"
                                     placeholder="Username"
                                     name="username"
+                                    autoComplete='true'
                                     ref={usernameRef}
+                                    id={uuidv4() + "-username"}
+                                   
                                 />
                             </Col>
-                            <Col className="px-0" xs={6}>
-                                <Button variant="warning" onClick={handleAddMember}>Add User</Button>
+                            <Col className={"px-0 ms-1 " }>
+                                <Button ref={addMemBtnRef} id="add-member" variant="warning" onClick={handleAddMember}><IoMdAdd size={25}></IoMdAdd></Button>
                             </Col>
                         </Row>
                         <Row>
@@ -122,18 +184,17 @@ const CreateGroup = () => {
                         </Row>
                         <Row>
                             <Col xs={{ span: 10, offset: 1 }}>
-                                <Button className="mt-5" type="submit" variant='warning' id="new-group">Create Group</Button>
+                                <Button ref={addSubmitBtnRef}className="my-5" type="submit" variant='warning' id="new-group">Create Group</Button>
                             </Col>
                         </Row>
                     </Form>
                 </Col>
-                <Col xs={12} md={{ span: 6, order: 'first' }} lg={{ span: 6, order: 'first' }} className={Styles.imageContainer}>
+                <Col sm={12} md={{ span: 6, order: 'first' }} lg={{ span: 6, order: 'first' }} className={Styles.imageContainer + " px-0"}>
                     <img src={Image} alt="image showing varieties of coffee"></img>
                 </Col>
             </Row>
         </Container>
     )
 }
-
-
 export default CreateGroup;
+
